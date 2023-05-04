573c68951d353d60ba8de0efa5bc38ffcb044616
Gabriel Cadet <phantomas@phantomas.xyz>
2023-05-04 08:58:04 +0200

    feat: Initial setup with nannou
    CHANGELOG:
    - Initialized cargo project
    - Created a simple window with nannou, using an empty model.

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use nannou::prelude::*;
+
+mod bin;
+use bin::Model;
+
+fn update(_: &App, _model: &mut Model, _update: Update) {}
+
+fn model(_: &App) -> Model {
+    Model {}
+}
+
+fn view(_: &App, _model: &Model, _frame: Frame) {}
+
+fn main() {
+    raytracer::hello_world();
+
+    nannou::app(model)
+        .update(update)
+        .simple_window(view)
+        .run();
+}
