3d8729658475d293ad9766c13d97d645072acbbc
Phantomas <phantomas@phantomas.xyz>
2023-05-09 09:21:57 +0200

    feat: update app to use the renderer

--- src/bin.rs
+++ src/bin.rs
@@ -1,1 +1,34 @@
-pub struct Model {}
+use nannou::wgpu::Texture;
+use raytracer::{
+    render::{Camera, Renderer},
+    scene::Scene,
+};
+
+pub struct Model {
+    pub renderer: Renderer,
+    pub camera: Camera,
+    pub scene: Scene,
+    pub texture: Option<Texture>,
+}
+
+impl Model {
+    pub fn new(renderer: Renderer, camera: Camera, scene: Scene) -> Self {
+        Self {
+            renderer,
+            camera,
+            scene,
+
+            texture: None,
+        }
+    }
+
+    pub fn empty() -> Self {
+        Self::new(Default::default(), Default::default(), Default::default())
+    }
+}
+
+impl Default for Model {
+    fn default() -> Self {
+        Self::empty()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,58 @@
-use nannou::prelude::*;
+use nannou::{image::DynamicImage, prelude::*};
 
 mod bin;
 use bin::Model;
+use raytracer::maths::Point;
+use raytracer::scene::objects::Sphere;
+use raytracer::scene::*;
 
 fn update(_: &App, _model: &mut Model, _update: Update) {}
 
-fn model(_: &App) -> Model {
-    Model {}
+fn model(app: &App) -> Model {
+    let scene = scene()
+        .with_object(Sphere::new(Point::new(0., 0., 100.), 50.))
+        .build();
+
+    app.new_window()
+        .size(512, 512)
+        .resizable(false)
+        .view(view)
+        .build()
+        .unwrap();
+
+    Model {
+        scene,
+        ..Default::default()
+    }
 }
 
-fn view(_: &App, _model: &Model, _frame: Frame) {}
+fn view(app: &App, model: &Model, frame: Frame) {
+    let (tx, ty) = app.main_window().inner_size_points();
+    let tx = tx as u32;
+    let ty = ty as u32;
+    //let [tx, ty] = frame.texture_size();
+
+    let mut img = DynamicImage::new_rgba8(tx, ty);
+
+    {
+        let rgba_img = img.as_mut_rgba8().unwrap();
+
+        for y in 0..ty {
+            for x in 0..tx {
+                let color = model
+                    .renderer
+                    .render_pixel(x, y, &model.camera, &model.scene);
+                rgba_img.put_pixel(x, y, color.to_array().into());
+            }
+        }
+    }
+
+    let texture = wgpu::Texture::from_image(app, &img);
+    let draw = app.draw();
+    draw.texture(&texture);
+    draw.to_frame(app, &frame).unwrap();
+}
 
 fn main() {
-    nannou::app(model).update(update).simple_window(view).run();
+    nannou::app(model).update(update).run();
 }
